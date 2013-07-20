880b460d416c48cb786c5a48afec690bd810b2e3
2007gmat@gmail.com <2007gmat@gmail.com>
2013-07-20 02:11:40 +0000

    using git commit -a

--- web.js
+++ web.js
@@ -1,13 +1,9 @@
-fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
-//var buffer = new Buffer(50);
- //var len = buffer.write(fs.readFileSync('./index.html', "utf8") );
-  response.send(fs.readFileSync('./index.html', "utf8"));
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
