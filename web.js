f78de1b7a0c9a242ee6f9684355af4b48fb99057
2007gmat@gmail.com <2007gmat@gmail.com>
2013-07-21 16:34:41 +0000

    redoing the HW3

--- web.js
+++ web.js
@@ -5,10 +5,9 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-var buffer = new Buffer(50);
- var len = buffer.write(fs.readFileSync('./index.html', "utf8"),0,"buffer.write(fs.readFileSync('./index.html', "utf8"),0,"ascii" );ascii" );
-  
-response.send(buffer.toString('utf8', 0, len));
+//var buffer = new Buffer(50);
+  response.send(fs.readFileSync('./index1.html', "utf8"));
+//response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
