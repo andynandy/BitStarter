26b8875f5283221b1f26e8f310ede4b96be417e7
2007gmat@gmail.com <2007gmat@gmail.com>
2013-07-21 16:12:04 +0000

    aa

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+var buffer = new Buffer(50);
+ var len = buffer.write(fs.readFileSync('./index.html', "utf8"),0,"buffer.write(fs.readFileSync('./index.html', "utf8"),0,"ascii" );ascii" );
+  
+response.send(buffer.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
